a0f070981411c894d2ace70b09f32b9cf016f700
ImmaCrayon81 <e0559120@u.nus.edu>
2022-02-27 17:36:51 +0800

    add navbar

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ export const App = () => {
   return (
     <div className='App'>
       <div className='gradient__bg'>
-      <h1>testing</h1>
         <Navbar />
         <Header />      
       </div>
